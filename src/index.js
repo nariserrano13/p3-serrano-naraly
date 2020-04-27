fff5651fed473731699fb688fdc264ed5f02094c
Naraly Serrano <naralycserrano@ufl.edu>
2020-04-26 20:30:17 -0400

    Add es6 app class and html structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 class App {
-    renderTemplate(){
-        
+    constructor (header, generator, footer){
+        this.header = header;
+        this.generator = generator;
+        this.footer = footer;
     }
 }
+
+const renderTemplate = new App ('this is the header', 'this is the generator', 'this is the footer');
+
+const template = `
+    <header>
+        <div>
+            <h1>${renderTemplate.header}</h1>
+        </div>
+        <figure></figure>
+    </header>
+    <main>
+        <div>${renderTemplate.generator}</div>
+        <div></div>
+    </main>
+    <footer>
+        <div>${renderTemplate.footer}</div>
+        <div></div>
+        <div></div>
+    </footer>
+`;
+
+document.body.innerHTML= template;
